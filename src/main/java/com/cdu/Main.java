0ab1040019827520ccfff3cd7875a068e05e177a
djl <1286574963@qq.com>
2024-08-20 21:10:16 +0800

    first

---
+++ src/main/java/com/cdu/Main.java
@@ -0,0 +1,7 @@
+package com.cdu;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/cdu/user.java
@@ -0,0 +1,4 @@
+package com.cdu;
+
+public class user {
+}
